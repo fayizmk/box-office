6b732800b8414125dbe38fbb58e127a41cb4e205
fayizmk <fayizmk20@gmail.com>
2021-11-12 19:33:04 +0530

    added layout management and created title

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import Navs from './components/Navs';
 import Home from './pages/Home';
 import Star from './pages/Star';
 
 function App() {
   return (
     <div>
-      <Navs />
       <Switch>
         <Route exact path="/">
           <Home />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { MainPageLayout } from '../components/MainPageLayout';
 
 const Home = () => {
-  return <div>this is homepage</div>;
+  return <MainPageLayout>this is homepage</MainPageLayout>;
 };
 
 export default Home;
