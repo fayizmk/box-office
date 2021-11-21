36a78b9defbf6cdf9ebe179fd7ea610baf46172e
fayizmk <fayizmk20@gmail.com>
2021-11-21 17:17:42 +0530

    finished Starred page

--- src/components/show/ShowMainData.jsx
+++ src/components/show/ShowMainData.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Star from '../../pages/Star';
+import { Star } from '../styled';
 import IMG_PLACEHOLDER from '../../images/not-found.png';
 import { Headline, MainDataWrapper, TagList } from './ShowMainData.styled';
 
@@ -10,6 +10,9 @@ const ShowMainData = ({ name, rating, summary, tags, image }) => {
       <div className="text-side">
         <Headline>
           <h1>{name}</h1>
+          <div>
+            <Star active />
+          </div>
           <div>
             <span>{rating.average || 'N/A'}</span>
           </div>
--- src/pages/Star.jsx
+++ src/pages/Star.jsx
@@ -1,8 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { MainPageLayout } from '../components/MainPageLayout';
+import { useShows } from '../misc/custom-hooks';
+import { apiGet } from '../misc/config';
+import ShowGrid from '../components/show/ShowGrid';
 
 const Star = () => {
-  return <MainPageLayout>this is starred</MainPageLayout>;
+  const [starred] = useShows();
+
+  const [shows, setShows] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (starred && starred.length > 0) {
+      const promises = starred.map(showId => apiGet(`/shows/${showId}`));
+      Promise.all(promises)
+        .then(apiData => apiData.map(show => ({ show })))
+        .then(results => {
+          setShows(results);
+          setIsLoading(false);
+        })
+        .catch(err => {
+          setError(err.message);
+          setIsLoading(false);
+        });
+    } else {
+      setIsLoading(false);
+    }
+  }, [starred]);
+
+  return (
+    <MainPageLayout>
+      {isLoading && <div> shows are still loaing</div>}
+      {error && <div> error occured: {error}</div>}
+      {!isLoading && !shows && <div>Nothing to display</div>}
+      {!isLoading && !error && shows && <ShowGrid data={shows} />}
+    </MainPageLayout>
+  );
 };
 
 export default Star;
